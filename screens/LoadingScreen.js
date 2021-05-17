38e53fdfcae8672469883972743b85842b33a980
Oleksa Sarnatskyi <OlSa@MacBook-Pro-username.local>
2021-05-17 17:28:31 +0300

    diagrams schemas

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -2,25 +2,35 @@ import React from 'react'
 import {View, Text, StyleSheet, ActivityIndicator, Image} from 'react-native'
 import * as firebase from 'firebase'
 
+
+
 export default class LoginScreen extends React.Component {
+
+  
   componentDidMount() {
-    firebase
-      .auth()
-      .onAuthStateChanged(user => {
-        this
-          .props
-          .navigation
-          .navigate(user
-            ? "App"
-            : "Auth");
+    firebase.auth().onAuthStateChanged(user => {
+        this.props.navigation.navigate(user ? "App" : "Auth");
       });
   }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loading...
-        </Text>
-        <Image source={require("../assets/coffeeBeansLoading.png")}></Image>
+        <View style={styles.divider}>
+          <Text style={styles.title}>
+            Cupping
+            <Text
+              style={{
+              fontWeight: "600",
+              color: "#D32705"
+            }}>
+              Lists
+            </Text>
+          </Text>
+        <Image source={require("../assets/coffeeBeansLoading.png")}style={styles.img}></Image>
+        <Text>Loading...</Text>
+        </View>
+        
         {/* <ActivityIndicator>
 
         </ActivityIndicator> */}
@@ -34,5 +44,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  divider: {
+    //backgroundColor: "#75C2E5",
+    marginTop: 60,
+    height: 1,
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "900",
+    color: "#002333",
+    // paddingHorizontal: 20
+    marginTop: 0
+  },
+  img:{
+    marginTop:50
   }
 })
+
